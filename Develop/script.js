837eeabe5ae41d2bc0d2c1b174464225b15eafd5
Sarah Dowd <sarahlynnedowd@gmail.com>
2020-10-31 16:22:49 -0400

    added comments to explain code

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,18 @@
-// Assignment code here
+// Create arrays that hold potential password criteria
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var UPPERCASE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var specialCharaters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '"', '-', '+', '=', ',', '.', ':', ';', '<', '>', '`', '~', '|', '{', '}', '/']
 
+// Array that holds user selected password criteria
 var selectedOptions = []
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
+//function that generates password
 function generatePassword () {
+  
   //prompt user for password criteria
   
   //prompt for complexity
@@ -56,6 +59,7 @@ function generatePassword () {
     }
   }
   console.log(selectedOptions);
+
   //logic of function  
   var password = '';
   for (var i = 0; i < complexity; i++) {
@@ -72,8 +76,6 @@ function writePassword() {
   passwordText.value = password;
     
 }
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
